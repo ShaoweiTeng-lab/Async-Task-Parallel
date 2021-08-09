5113f9c306ed67331e3cd2b2979255068fd8dc1a
Jason_Teng <50354880+sdonline13@users.noreply.github.com>
2021-08-09 12:03:58 +0800

    add Thread.Sleep vs Task.Delay

--- Assets/Jason/Scripts/TaskManager.cs
+++ Assets/Jason/Scripts/TaskManager.cs
@@ -189,9 +189,11 @@ public class TaskManager : MonoBehaviour
         await Task.Run(
             () =>
             {
-
+                //http://slashview.com/archive2016/20160201.html
                 count++;
                 Debug.Log($"{DateTime.Now:yyyy-MM-dd HH:mm:ss}_Do work. count = {count}");
+                //Thread.Sleep就是對本身自己這個執行緒，進行睡眠的功能。而反觀Task.Delay是指，在本身這個執行緒下再去生出一個新的執行緒，並對這個執行緒做時間計算。因此Task.Delay如果是要用在延遲的作用上，那麼就得要在前面加上個await才會有效
+                //Task.Delay(3000);//無效 且如果要加上await 則不能放在 lamda同步中 會錯 因此用Thread.Sleep();
                 Thread.Sleep(3000);
                 //if (count < 1)//限制條件 只執行一次
                 //{
