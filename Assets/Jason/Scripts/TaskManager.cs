a0f3d611d2ba0018e1d3add5915b9d69bbc2b968
Jason_Teng <50354880+sdonline13@users.noreply.github.com>
2021-08-09 10:31:13 +0800

    add Task的Wait/WaitAny/WaitAll方法

--- Assets/Jason/Scripts/TaskManager.cs
+++ Assets/Jason/Scripts/TaskManager.cs
@@ -18,29 +18,55 @@ public class TaskManager : MonoBehaviour
         //Task tastVoid = new Task(Test);
         //t1.Start();
         //tastVoid.Start();
-        //======================================
+        //=================Task創建方式======================================================================================
         //Task創建方式有三種 ,ThreadPool不能控制線程的執行順序，我們也不能獲取線程池內線程取消/異常/完成的通知，不能有效監控和控制線程池中的線程
         //1.new方式實例化一個Task，需要通過Start方法啟動
-        Task task = new Task(() =>
-        {
-            Thread.Sleep(100);
-            Debug.Log($"hello, task1的線程ID為{Thread.CurrentThread.ManagedThreadId}");
-        });
-        task.Start();
+        //Task task = new Task(() =>
+        //{
+        //    Thread.Sleep(100);
+        //    Debug.Log($"hello, task1的線程ID為{Thread.CurrentThread.ManagedThreadId}");
+        //});
+        //task.Start();
 
-        //2.Task.Factory.StartNew(Action action)創建和啟動一個Task
-        Task task2 = Task.Factory.StartNew(() =>
-        {
+        ////2.Task.Factory.StartNew(Action action)創建和啟動一個Task
+        //Task task2 = Task.Factory.StartNew(() =>
+        //{
+        //    Thread.Sleep(100);
+        //    Debug.Log($"hello, task2的線程ID為{ Thread.CurrentThread.ManagedThreadId}");
+        //});
+
+        ////3.Task.Run(Action action)將任務放在線程池隊列，返回並啟動一個Task
+        //Task task3 = Task.Run(() =>
+        //{
+        //    Thread.Sleep(100);
+        //    Debug.Log($"hello, task3的線程ID為{ Thread.CurrentThread.ManagedThreadId}");
+        //});
+
+
+        //===========Task的延續操作(WhenAny/WhenAll/ContinueWith) 實現阻塞線程===============================================================
+        /*
+         Wait/WaitAny/WaitAll方法返回值為void，這些方法單純的實現阻塞線程。我們現在想讓所有task執行完畢(或者任一task執行完畢)後，開始執行後續操作，這時就可以用到WhenAny/WhenAll方法，
+         這些方法執行完成返回一個task實例。task.WhenAll(Task[] tasks) 表示所有的task都執行完畢後再去執行後續的操作， task.WhenAny(Task[] tasks) 表示任一task執行完畢後就開始執行後續操作
+        */
+        Task task1 = new Task(() => {
+            Debug.Log("進入線程1 準備 sleep");
+            Thread.Sleep(500);
+            Debug.Log("線程1執行完畢！");
+        });
+        task1.Start();
+        Task task2 = new Task(() => {
+            Debug.Log("進入線程2 準備 sleep");
+            Thread.Sleep(3000);
+            Debug.Log("線程2執行完畢！");
+        });
+        task2.Start();
+        //task1，task2執行完了後執行後續操作
+        Task.WhenAll(task1, task2).ContinueWith((t) => {
+            Debug.Log("進入線程3 準備 sleep");
             Thread.Sleep(100);
-            Debug.Log($"hello, task2的線程ID為{ Thread.CurrentThread.ManagedThreadId}");
+            Debug.Log("執行後續操作完畢！");
         });
 
-        //3.Task.Run(Action action)將任務放在線程池隊列，返回並啟動一個Task
-        Task task3 = Task.Run(() =>
-        {
-            Thread.Sleep(100);
-            Debug.Log($"hello, task3的線程ID為{ Thread.CurrentThread.ManagedThreadId}");
-        });
     }
 
     // Update is called once per frame
